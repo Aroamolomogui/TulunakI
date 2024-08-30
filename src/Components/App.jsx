e901c817a757616d7830880d437580b8ad2cad0f
Aroa Pavón García <aropavongarcia@gmail.com>
2024-08-31 01:30:18 +0200

    Header first commit whit description
    the underline is still not visible, continue and put more spaces to align the divs to the sides

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,15 @@
 
+import Header from './Header';
 
 function App() {
   return (
-    <>
+    <div>
+    <Header/>
+      
        <p className="text-red-500">Template</p>
-    </>
-  )
+
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="bg-white text-black">
+      <div className="container mx-auto px-4 py-4">
+        <div className="flex justify-between items-center">
+
+          <div className="flex items-center space-x-2">
+          {/* posible icono para completar el logo con el nombre de la app */}
+            <span className="text-2xl font-bold">TulunakI
+              <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-violet-500
+               transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+              </span>
+              </div>
+
+            <nav className="hidden md:flex items-center space-x-6">
+              <a href="/Miluna" className="text-lg font-semibold hover:text-violet-500 transition duration-300 relative-group">
+              <span className="relative"> 
+                Mi luna <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-violet-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+              </span>
+            </a>
+
+              <a href="/Equilibrio emocional" className="text-lg font-semibold hover:text-violet-500 transition duration-300 relative-group">
+                <span className="realtive"> Equilibrio emocional 
+                <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-yellow-300 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+              </span>
+            </a>
+
+            <a href="/Contacto" className="text-lg font-semibold hover:text-violet-500 transition duration-300 relative-group">
+            <span className="relative">
+              Contacto <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-black transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+              </span>
+            </a>
+            </nav>
+
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </header>
+
+  )
+  
 }
 
 export default Header;
