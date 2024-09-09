e3d5c3f34ed1aa1623b99e45c5c2515f89fdaf4e
Aroa Pavón García <aropavongarcia@gmail.com>
2024-09-09 13:25:06 +0200

    add html and css to container ppal read to description
    I add several elements to the form such as the main container, the hidden moon sign section, I prepare the img card, and I add the container for the title and a brief description of the form

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,12 @@
 
 import Header from './Header';
+import Form from './Form';
 
 function App() {
   return (
     <div>
     <Header/>
+    <Form/>
       
        <p className="text-red-500">Template</p>
 
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Form() {
-  return <div>Form</div>;
+  return <div className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
+    <div className="mb-8 text-center hidden">
+      <img src="" alt="" />
+      <h2 className="mt-4 text-2xl font-bold">Tu signo lunar es</h2>
+      </div>
+
+    <div className="max-w-2x1 w-full space-y-8 p-8 rounded-xl shadow-2xl">
+      <div className="text-center">
+    <h2 className="mt-4 text-2xl font-bold text-black"
+    >Descubre tu signo lunar</h2>
+    <p className="mt-2 text-base text-gray-200">Ingresa tus datos para revelar los secretos de tu luna</p>
+    </div>
+    </div>
+
+    <div>
+      <form action=""></form>
+    </div>
+
+    </div>;
 }
 
 export default Form;
