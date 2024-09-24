d258193a24679fb2dc8b762a82b3776559516e48
Aroa Pavón García <aropavongarcia@gmail.com>
2024-09-24 13:42:51 +0200

    Add Hero
    Creo una nueva rama para el hero, creo e importo el componente en el ppal para utilizarlo, añado estilos básicos del css queda iterar mejoras.

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Hero() {
+  return <div className=" bg-gradient-to-r from-purple-500 to-indigo-600 text-white py-20">
+    <div className="container mx-auto px-4">
+        <h1 className="text-4xl md:text-5xl font-bold mb-4 text-center">
+            Cada vínculo es como una plantita, si lo cuidas crece.
+        </h1>
+        <p className="text-xl md:text-2xl text-center max-w-2x1 mx-auto">
+            Hazte caso cuando algo no te vibra.
+        </p>
+
+    </div>
+
+  </div>;
+}
+
+export default Hero;
