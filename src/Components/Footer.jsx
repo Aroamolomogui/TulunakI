e538a97db43d18a262edc118af9a73fee496987f
Aroa Pavón García <aropavongarcia@gmail.com>
2024-09-25 09:40:15 +0200

    iconos footer y ancho de la pantalla ajsutados
    El footer no ocupaba todo el ancho de la pantalla, cambie el grid por el flex para tener más control en la distribución de las columnas, añadí `w-full md:w-1/3` para ocupar todo el ancho en móviles y un tercio en pantallas más grandes. y `mb-8 md:mb-0` a las dos primeras columnas para dar espacio entre ellas en móviles. los iconos no tenían el mismo tamaño, asi que pasé por props el tamaño para que quedasen iguales.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,14 +6,15 @@ import { FaTiktok } from "react-icons/fa6";
 
 
 function Footer() {
-  return <footer className=" text-black">
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        <div>
+  return <footer className=" text-black w-full p-12">
+    <div className="w-full px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full">
+        <div className="w-full md:w-1/3 mb-8 md:mb-0">
           <h3 className="text-lg font-semibold mb-4">Tulunaki Astrología</h3>
           <p className="text-sm text-gray-300">Descubre los secretos del universo y encuentra tu camino en las estrellas.</p>
+        
         </div>
-        <div>
+        <div className="w-full md:w-1/3 mb-8 md:mb-0 md:text-center">
           <h3 className="text-lg font-semibold mb-4">Enlaces rápidos</h3>
           <ul className="space-y-2">
             <li><a href="/" className="text-sm hover:text-white transition-colors">TulunakI</a></li>
@@ -23,19 +24,20 @@ function Footer() {
           </ul>
           </div>
 
-          <div>
+          <div className="w-full md:w-1/3 md:text-right">
             <h3 className="text-lg font-semibold mb-4">Síguenos</h3>
-            <div className="flex space-x-4">
-              <a href=""><Instagram /></a>
-              <a href=""><FaTiktok /></a>
+            <div className="flex md:justify-end space-x-4">
+              <a href="https://www.instagram.com/aroamolomogi/" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors group" aria-label="Visita el perfil de Instagram de Aroamolomogi" ><Instagram size={24} className="group-hover:scale-110 transition-transform"/></a>
+
+              <a href="https://www.tiktok.com/@aroamolomogui" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors" aria-label="Visita nuestro perfil de TikTok"><FaTiktok size={24} /></a>
               
             </div>
           
         </div>
       </div>
 
-      <div className="mt-8 pt-8 border-t border-black text-center text-sm text-gray-300"></div>
-      <p>&copy; {new Date().getFullYear()} TulunakI Astrología. Todos los derechos reservados.</p>
+      <div className="mt-8 pt-8 border-t border-black flex justify-center "></div>
+      <p className="text-sm text-black">&copy; {new Date().getFullYear()} TulunakI Astrología. Todos los derechos reservados.</p>
     </div>
 
   </footer>;
